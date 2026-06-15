8f1bb0ecc144784ce93a4032497cbba64830f800
techytonys <techytony@icloud.com>
2026-06-15 06:57:57 -0500

    Update gush to v2.1

--- Formula/gush.rb
+++ Formula/gush.rb
@@ -2,7 +2,7 @@ class Gush < Formula
   desc "Push code to GitHub in one command"
   homepage "https://gushpush.com"
   url "https://raw.githubusercontent.com/techytonys/Gush/main/gush.sh"
-  version "2.0"
+  version "2.1"
   sha256 "1fa10d367a8f2e91ac359aef699978a8bfcca66ddc8bae4c86efb35ffa2538aa"
 
   depends_on "gh"
